b94b291cbaff21abe0b4652f49e9939fc1b3fb68
David Lexe <David_Lexe@hotmail.com>
2025-03-26 16:01:14 +0100

    ActivityService can now:
    Edit specific activities
    Delete activities
    Fetch a specific activity from ID.

--- backend/backend/app/src/main/java/com/smartcalender/app/service/ActivityService.java
+++ backend/backend/app/src/main/java/com/smartcalender/app/service/ActivityService.java
@@ -3,13 +3,17 @@ package com.smartcalender.app.service;
 import com.smartcalender.app.entity.Activity;
 import com.smartcalender.app.repository.ActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class ActivityService {
+    private List<Activity> activities;
 
     private final ActivityRepository activityRepository;
 
@@ -23,6 +27,30 @@ public class ActivityService {
         //Couldn't really piece this together.
 
         return activityRepository.save(activity);
+    }
+
+    public ResponseEntity<Boolean> deleteActivity(Activity activity) {
+        activityRepository.delete(activity);
+        return new ResponseEntity<>(true, HttpStatus.OK); //TODO - return-message should be based on if the database was successful or not
+    }
+
+    public ResponseEntity<Activity> editActivity(Activity activity) {
+        Optional<Activity> activityOptional = activityRepository.findById(activity.getId());
+        if (activityOptional.isPresent()) {
+            Activity activityToEdit = activityOptional.get();
+
+            activityToEdit.setName(activity.getName());
+            activityToEdit.setDescription(activity.getDescription());
+            activityToEdit.setStartTime(activity.getStartTime());
+            activityToEdit.setEndTime(activity.getEndTime());
+            activityRepository.save(activityToEdit);
+            return new ResponseEntity<>(activity, HttpStatus.OK);
+        }
+        return null;
+    }
+
+    public Optional<Activity> getActivity(long id) {
+        return activityRepository.findById(id);
     }
 
     public List<Activity> getOngoingActivities() {
