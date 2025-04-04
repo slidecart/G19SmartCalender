e3ac42242bf39b248b4e3c5c64f3178fc49b0ff0
cs <eseoics@gmail.com>
2025-04-04 15:07:11 +0200

    fixat footer hell yea

--- frontend/src/components/containers/header.jsx
+++ frontend/src/components/containers/header.jsx
@@ -6,7 +6,7 @@ function Header(){
     return(
         <Box sx={{backgroundColor: "#0077ff7e", py: 1.5, display: "flex"}}>
             <Typography variant="h1" sx={{ mx: 1 }}>
-                Smartcalendar
+                SmartCalendar
             </Typography>
             <Navbar/>
         </Box>
