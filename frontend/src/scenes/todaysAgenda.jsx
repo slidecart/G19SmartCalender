0871ecb0f9c0747a7862cfb0727e500337cb6bcc
Linnea Värnström <linnea.varnstrom@outlook.com>
2025-04-04 15:08:05 +0200

    Update todaysAgenda.jsx

--- frontend/src/scenes/todaysAgenda.jsx
+++ frontend/src/scenes/todaysAgenda.jsx
@@ -1,11 +1,12 @@
 import { Box, Container, Typography } from "@mui/material";
 import Body from "../components/containers/body";
+import WeeklyCalendar from "../components/calendar/weeklyCalendar";
 
 function TodaysAgenda(){
 
     return(
         <Body>
-            
+            <WeeklyCalendar/>
         </Body>
     )
 }
