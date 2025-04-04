360bebe86f940aefb0be9743f78ac2a4ea3441ea
cs <eseoics@gmail.com>
2025-04-04 11:54:20 +0200

    yes äntligen lagt till footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/containers/header";
 import LogIn from "./scenes/login";
+import Footer from"./components/containers/footer";
 
 function App() {
   return (
     <>
       <Header/>
       <LogIn/>
+      <Footer/>
     </>
   );
 
--- frontend/src/components/containers/footer.jsx
+++ frontend/src/components/containers/footer.jsx
@@ -1,1 +1,15 @@
-{/*Fixa en footer som ska tilläggas till alla sidor*/}
+import { Box, Typography } from "@mui/material";
+
+function Footer(){
+
+    return(
+        <Box sx={{backgroundColor: "blue", py: 0.5, display: "flex"}}>
+            <Typography variant="h1">
+                Copyright 2025
+            </Typography>
+
+        </Box>
+    )
+}
+
+export default Footer;
