3e6f9e26b12a553767646d4acb354a553c4f4a24
David Lexe <David_Lexe@hotmail.com>
2025-04-30 16:26:18 +0200

    Removed TaskGrid

--- frontend/src/scenes/taskGrid.jsx
+++
@@ -1,8 +0,0 @@
-import { Container, Accordion, AccordionSummary, AccordionDetails, Typography, Stack, Button, IconButton, Box } from "@mui/material";
-
-
-const taskGrid = ( {tasks}) => {
-    return(
-        <Container></Container>
-    )
-}
