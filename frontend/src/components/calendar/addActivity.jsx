3fa112f9006a517daee7ba35d386ab35f623a950
Carl Lundholm <carl_0221@hotmail.se>
2025-05-05 14:21:36 +0200

    Add category selection to AddActivity dialog and refactor CreateCategoryRequest

--- frontend/src/components/calendar/addActivity.jsx
+++ frontend/src/components/calendar/addActivity.jsx
@@ -1,36 +1,32 @@
-import { Dialog, DialogTitle, DialogContent, DialogActions, TextField, Button, Box } from "@mui/material";
+import { Dialog, DialogTitle, DialogContent, DialogActions, TextField, Button, Box, FormControl, InputLabel, Select, MenuItem } from "@mui/material";
 
-const AddActivity = ({ open, onClose, formData, handleChange, handleSubmit, mode }) => {
+const AddActivity = ({ open, onClose, formData, handleChange, handleSubmit, mode, categories, onCreateCategory }) => {
     const isEditMode = mode === "edit";
 
     return(
         <Dialog open={open} onClose={onClose}>
-
-            {/* Title for the dialog */}
             <DialogTitle m={1}>
                 {isEditMode ? "Redigera aktivitet" : "Lägg till aktivitet"}
             </DialogTitle>
-
-            {/* Dialog content allowing the user to enter information about created activity */}
             <DialogContent sx={{width:"400px", ml:1}}>
-                <Box mb ={2}>
-                    <TextField 
-                        name="name" 
-                        label="Titel" 
-                        value={formData.name} 
-                        onChange={handleChange} 
+                <Box mb={2}>
+                    <TextField
+                        name="name"
+                        label="Titel"
+                        value={formData.name}
+                        onChange={handleChange}
                         required
                     />
                 </Box>
-                <Box mb ={2}>
-                    <TextField 
-                        name="description" 
-                        label="Beskrivning" 
-                        value={formData.description} 
+                <Box mb={2}>
+                    <TextField
+                        name="description"
+                        label="Beskrivning"
+                        value={formData.description}
                         onChange={handleChange}
                     />
                 </Box>
-                <Box mb ={2}>
+                <Box mb={2}>
                     <TextField
                         name="location"
                         label="Plats"
@@ -38,44 +34,76 @@ const AddActivity = ({ open, onClose, formData, handleChange, handleSubmit, mode
                         onChange={handleChange}
                     />
                 </Box>
-                <Box mb ={2}>
-                    <TextField 
-                        name="date" 
-                        label="Datum" 
-                        type="date" 
-                        InputLabelProps={{shrink: true}} 
-                        value={formData.date} 
-                        onChange={handleChange} 
+                <Box mb={2}>
+                    <FormControl fullWidth>
+                        <InputLabel id="category-select-label">Kategori</InputLabel>
+                        <Select
+                            labelId="category-select-label"
+                            id="category-select"
+                            name="categoryId"
+                            value={formData.categoryId}
+                            onChange={handleChange}
+                            label="Kategori"
+                        >
+                            {categories && categories.length > 0 ?
+                                categories.map(category => (
+                                    <MenuItem key={category.id} value={category.id}>
+                                        {category.name}
+                                    </MenuItem>
+                                ))
+                                :
+                                <MenuItem disabled value="">
+                                    <Box display="flex" justifyContent="space-between" alignItems="center" width="100%">
+                                        <span>No categories created</span>
+                                        <span
+                                            style={{color:"blue", textDecoration:"underline", cursor:"pointer"}}
+                                            onClick={onCreateCategory}
+                                        >
+                                            Create a category
+                                        </span>
+                                    </Box>
+                                </MenuItem>
+                            }
+                        </Select>
+                    </FormControl>
+                </Box>
+                <Box mb={2}>
+                    <TextField
+                        name="date"
+                        label="Datum"
+                        type="date"
+                        InputLabelProps={{shrink: true}}
+                        value={formData.date}
+                        onChange={handleChange}
                         required
                     />
-                </Box>                
-                <Box mb ={2}>
-                    <TextField 
-                        name="startTime" 
-                        label="Starttid"   
-                        type="time" 
-                        InputLabelProps={{shrink: true}} 
-                        value={formData.startTime} 
-                        onChange={handleChange} 
+                </Box>
+                <Box mb={2}>
+                    <TextField
+                        name="startTime"
+                        label="Starttid"
+                        type="time"
+                        InputLabelProps={{shrink: true}}
+                        value={formData.startTime}
+                        onChange={handleChange}
                         required
                     />
-                </Box>                
-                <Box mb ={2}>
+                </Box>
+                <Box mb={2}>
                     <TextField
                         name="endTime"
                         label="Sluttid"
                         type="time"
-                        InputLabelProps={{ shrink: true }} 
+                        InputLabelProps={{ shrink: true }}
                         value={formData.endTime}
                         onChange={handleChange}
                         required
                     />
-                </Box>                
+                </Box>
             </DialogContent>
-
             <DialogActions>
-                <Button onClick = {onClose}>Avbryt</Button>
-                <Button onClick = {handleSubmit} variant="contained">Spara</Button>
+                <Button onClick={onClose}>Avbryt</Button>
+                <Button onClick={handleSubmit} variant="contained">Spara</Button>
             </DialogActions>
         </Dialog>
     );
