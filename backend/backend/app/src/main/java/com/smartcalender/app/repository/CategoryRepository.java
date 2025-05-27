2cf8b3d3c65116d926625f179a30f9017f680b89
Carl Lundholm <carl_0221@hotmail.se>
2025-05-27 16:59:09 +0200

    Refactor category repository and service to associate categories with users and not only category name when searching.

--- backend/backend/app/src/main/java/com/smartcalender/app/repository/CategoryRepository.java
+++ backend/backend/app/src/main/java/com/smartcalender/app/repository/CategoryRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     List<Category> findByUser(User user);
     Optional<Category> findByIdAndUser(Long id, User user);
-    Optional<Category> findByName(String categoryName);
+    Optional<Category> findByNameAndUser(String name, User user);
 }
