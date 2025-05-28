4ce85a59c748298e0204a726c4f6f54d8f908055
David Lexe <David_Lexe@hotmail.com>
2025-05-28 09:53:21 +0200

    Enable WAR packaging and configure SpringBootServletInitializer
    Updated `pom.xml` to support WAR packaging and added the necessary configuration in `AppApplication` by extending `SpringBootServletInitializer`. These changes enable deployment to servlet containers such as Tomcat.

--- backend/backend/app/src/main/java/com/smartcalender/app/AppApplication.java
+++ backend/backend/app/src/main/java/com/smartcalender/app/AppApplication.java
@@ -2,6 +2,8 @@ package com.smartcalender.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,7 +12,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EnableAsync
 @EnableScheduling
-public class AppApplication {
+public class AppApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(AppApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
