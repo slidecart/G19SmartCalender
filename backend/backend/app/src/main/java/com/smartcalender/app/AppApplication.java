1043e7089b9c5ef3b45575a5193f40479a99903d
Carl Lundholm <carl_0221@hotmail.se>
2025-05-28 11:00:35 +0200

    Switch packaging from WAR to JAR and remove servlet initializer.
    Updated the Maven packaging to JAR as the application no longer requires deployment as a WAR file but Jar file. Removed the SpringBootServletInitializer methods since they are unnecessary for the standalone JAR configuration.

--- backend/backend/app/src/main/java/com/smartcalender/app/AppApplication.java
+++ backend/backend/app/src/main/java/com/smartcalender/app/AppApplication.java
@@ -2,8 +2,6 @@ package com.smartcalender.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,12 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EnableAsync
 @EnableScheduling
-public class AppApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
-		return application.sources(AppApplication.class);
-	}
+public class AppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
